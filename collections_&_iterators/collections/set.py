9eee14d3d7011d6c7acb2b1fb14ec171e313c2b0
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-07-04 22:14:08 +0300

    feat: add demonstration script for set operations including add, discard, and pop
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

---
+++ collections_&_iterators/collections/set.py
@@ -0,0 +1,12 @@
+raffle = {"James", "Denis", "Simon"}
+
+raffle.add("Daniel")
+raffle.add("Denis")
+print(raffle)
+
+raffle.discard("Simon")
+print(raffle)
+
+winner = raffle.pop()
+print(winner)
+print(raffle)
