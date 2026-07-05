fa21aba1714648925ab4152effb67780c15c360e
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-07-05 13:26:43 +0300

    feat: add length.py script to demonstrate empty list boolean evaluation
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

---
+++ collections_&_iterators/collections/length.py
@@ -0,0 +1,11 @@
+def main():
+  customers = []
+  if customers:
+    print("There are customers")
+  else:
+    print("Quiet day")
+  
+  print(bool(customers))
+
+if __name__ == "__main__":
+  main()
