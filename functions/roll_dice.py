6c248cb4a0f64e374b15da7d48fe66c41c2edfc4
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-07-11 21:11:08 +0300

    refactor: update roll_dice functions to accept variable dice arguments and add recursive dice cup implementation
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- functions/roll_dice.py
+++ functions/roll_dice.py
@@ -1,20 +1,26 @@
 import random
 
-def roll_dice(sides=6, dice=1) -> tuple[int, ...]:
-  return tuple(random.randint(1, sides) for _ in range(dice))
+def roll_dice(*dice: tuple[int, ...]) -> tuple[int, ...]:
+  return tuple(random.randint(1, d) for d in dice)
 
-def roll_dice_recursive(sides: int, dice: int) -> tuple[int, ...]:
-  if dice < 1:
-    return ()
+def roll_dice_recursive(*dice: tuple[int, ...]) -> tuple[int, ...]:
+  if dice:
+    roll = random.randint(1, dice[0])
+    return (roll,) + roll_dice_recursive(*dice[1:])
   
-  roll = random.randint(1, sides)
-  return (roll,) + roll_dice_recursive(sides, dice - 1)
+  return ()
 
-def main():
+def cup_of_dice():
+  dice_cup = roll_dice_recursive(6, 6, 6, 6, 6)
+  print(dice_cup)
+
+  bunch_o_dice = roll_dice(20, 6, 8, 4)
+  print(bunch_o_dice)
+
+def players():
   print("Roll for initiative...")
-  max_val = 20
-  player1, player2 = roll_dice(sides=max_val, dice=2)
-  player3, *_ = roll_dice(dice=5)
+  player1, player2 = roll_dice(4, 5)
+  player3, *_ = roll_dice(20)
   print(f"Player 3 rolled {player3}")
   
   if player1 >= player2:
@@ -22,5 +28,8 @@ def main():
   else:
     print(f"Player 2 goes first (rolled {player2}).")
 
+def main():
+  # players()
+  cup_of_dice()
 if __name__ == "__main__":
   main()
