de557865c5be0d5bce75a1b1e5bbae83b06db6e5
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-08-01 15:12:33 +0300

    feat: implement dependency inversion principle example with Manager and Worker classes
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

---
+++ oop/solid_principles/dependency_inversion.py
@@ -0,0 +1,16 @@
+from .interface_segragation import Worker, Human, Robot
+
+class Manager:
+  def assign_work(worker: Worker, duration: int, item: str) -> None:
+    worker.work(duration, item)
+
+def main():
+  dave = Human("Dave")
+  robot = Robot("i_9000")
+  manager = Manager()
+
+  manager.assign_work(dave, 5, "data aggregation")
+  manager.assign_work(robot, 10, "car disassembly")
+
+if __name__ == "__main__":
+  main()
