3fd5583f3f1a5f199a5c3391476cbfe5d4e08f49
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-08-03 14:16:47 +0300

    refactor: replace bitwise_shift.py with bitwise_operations.py to include additional bitwise operator examples
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

---
+++ binary_&_serialization/bitwise_operations.py
@@ -0,0 +1,11 @@
+# AND
+print(bin(0b1101 & 0b1010))
+print(bin(0b1101 | 0b1010))
+print(bin(0b1101 ^ 0b1010))
+print(bin(~0b0101 & 0b1111))
+print(f"{'=' * 10}")
+
+# shift
+print(bin(0b1100 << 4))
+print(bin(0b1100 >> 4))
+print(bin(-0b1100 >> 4))
--- binary_&_serialization/bitwise_shift.py
+++
@@ -1,3 +0,0 @@
-print(bin(0b1100 << 4))
-print(bin(0b1100 >> 4))
-print(bin(-0b1100 >> 4))
